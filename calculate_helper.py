477ff71a9cd82ae2bec80b1cf75d92d9bc886364
parkbible <p8yft8903@gmail.com>
2022-03-12 05:12:13 +0900

    약간 수정

--- calculate_helper.py
+++ calculate_helper.py
@@ -18,16 +18,14 @@ eval_List1 = [0] * 8
 multply_List1 = [10000, 5000, 1000, 500, 100]
 
 # 함수 내에서 바로 반영시켜줘야한다.
-def cal(i):
-    if i == "total":
-        e1['text'] = "총 금액 : " + str(sum(eval_List1))
-    elif i != 3 :
+def cal():
+    for i in range(3):
         eval_List1[i] = eval(entry_list1[i].get())
         label2_List1[i]['text'] = "금액 : " + str(eval_List1[i])
-    else :
-        for j in range(3, 8):
-            eval_List1[j] = multply_List1[j-3] * eval(entry_list1[j].get())
-            label2_List1[j]['text'] = "금액 : " + str(eval_List1[j])
+    for j in range(3, 8):
+        eval_List1[j] = multply_List1[j-3] * eval(entry_list1[j].get())
+        label2_List1[j]['text'] = "금액 : " + str(eval_List1[j])
+    e1['text'] = "총 금액 : " + str(sum(eval_List1))
 
 def init1():
     for i in range(len(eval_List1)):
@@ -42,20 +40,15 @@ for i in range(8):
     label_List1[i] = Label(wd, text=message_List1[i]).grid(row=1+i, column=0)
     entry_list1[i] = Entry(wd)
     entry_list1[i].insert(0, 0)
-    if i == 0 or i == 1 or i == 2:
-        btn_List1[i] = Button(wd, text="계산", command=partial(cal, i)).grid(row=1+i, column=2)
-        label2_List1[i] = Label(wd, text="금액 : ")
-    else:
-        label2_List1[i] = Label(wd, text="금액 : ")
+    label2_List1[i] = Label(wd, text="금액 : ")
 
 for i in range(8):
     label2_List1[i].grid(row=1+i, column=3)
     entry_list1[i].grid(row=1+i, column=1)
 
 a1 = Button(wd, text="초기화", command=init1).grid(row=9, column=0)
-b1 = Button(wd, text="현금 합산 계산", command=partial(cal,3)).grid(row=9, column=1)
 e1 = Label(wd, text="총 금액 : ")
-b1_2 = Button(wd, text="총액 계산", command=partial(cal,"total")).grid(row=9, column=2)
+b1_2 = Button(wd, text="총액 계산", command=cal).grid(row=9, column=1)
 e1.grid(row=9, column=3)
 
 
@@ -71,6 +64,7 @@ def cal2():
         eval_List2[i] = eval(entry_list2[i].get("1.0", END))
         label2_List2[i]['text'] = "금액 : " + str(eval_List2[i])
     e2['text'] = "총 금액 : " + str(sum(eval_List2))
+    e2_2['text'] = "이전타임 + 총 금액 : " + str(sum(eval_List2) + sum(eval_List1))
     
 for i in range(3):
     label_List2[i] = Label(wd, text=message_List2[i]).grid(row=11+i, column=0)
@@ -79,15 +73,17 @@ for i in range(3):
     label2_List2[i] = Label(wd, text="금액 : ")
 
 e2 = Label(wd, text="총 금액 : ")
+e2_2 = Label(wd, text="이전타임 + 총 금액 : ")
 b2 = Button(wd, text="총액 계산", command=cal2).grid(row=18, column=1)
 e2.grid(row=18, column=3)
+e2_2.grid(row=19, column=3)
 
 for i in range(3):
     entry_list2[i].grid(row=11+i, column=1)
     label2_List2[i].grid(row=11+i, column=3)
 
 
-Label(wd, text="마감시 계산").grid(row=19, column=0, pady=10)
+Label(wd, text="마감시 계산").grid(row=20, column=0, pady=10)
 
 label_List3 = [None] * 8
 entry_list3 = [None] * 8
@@ -107,17 +103,17 @@ multply_List3 = [10000, 5000, 1000, 500, 100]
 # b4 = Button(wd, text="현금 합산 계산").grid(row=28, column=1)
 # e3 = Label(wd, text="총 금액 : ").grid(row=28, column=3)
 
+
 # 수정 시작
-def cal3(i):
-    if i == "total":
-        e3['text'] = "총 금액 : " + str(sum(eval_List3))
-    elif i != 3 :
+def cal3():
+    for i in range(3):
         eval_List3[i] = eval(entry_list3[i].get())
         label2_List3[i]['text'] = "금액 : " + str(eval_List3[i])
-    else :
-        for j in range(3, 8):
-            eval_List3[j] = multply_List3[j-3] * eval(entry_list3[j].get())
-            label2_List3[j]['text'] = "금액 : " + str(eval_List3[j])
+    for j in range(3, 8):
+        eval_List3[j] = multply_List3[j-3] * eval(entry_list3[j].get())
+        label2_List3[j]['text'] = "금액 : " + str(eval_List3[j])
+    e3['text'] = "총 금액 : " + str(sum(eval_List3))
+
 
 def init3():
     for i in range(len(eval_List1)):
@@ -128,24 +124,19 @@ def init3():
     e3['text'] = "총 금액 : " + "0"
 
 for i in range(8):
-    label_List3[i] = Label(wd, text=message_List3[i]).grid(row=20+i, column=0)
+    label_List3[i] = Label(wd, text=message_List3[i]).grid(row=21+i, column=0)
     entry_list3[i] = Entry(wd)
     entry_list3[i].insert(0, 0)
-    if i == 0 or i == 1 or i == 2:
-        btn_List3[i] = Button(wd, text="계산", command=partial(cal3, i)).grid(row=20+i, column=2)
-        label2_List3[i] = Label(wd, text="금액 : ")
-    else:
-        label2_List3[i] = Label(wd, text="금액 : ")
+    label2_List3[i] = Label(wd, text="금액 : ")
 
 for i in range(8):
-    label2_List3[i].grid(row=20+i, column=3)
-    entry_list3[i].grid(row=20+i, column=1)
+    label2_List3[i].grid(row=21+i, column=3)
+    entry_list3[i].grid(row=21+i, column=1)
 
-a3 = Button(wd, text="초기화", command=init3).grid(row=28, column=0)
-b3 = Button(wd, text="현금 합산 계산", command=partial(cal3,3)).grid(row=28, column=1)
+a3 = Button(wd, text="초기화", command=init3).grid(row=29, column=0)
 e3 = Label(wd, text="총 금액 : ")
-b3_2 = Button(wd, text="총액 계산", command=partial(cal3,"total")).grid(row=28, column=2)
-e3.grid(row=28, column=3)
+b3_2 = Button(wd, text="총액 계산", command=cal3).grid(row=29, column=1)
+e3.grid(row=29, column=3)
 #수정 끝
 
 wd.mainloop()
